5646cc143b0f084967a8f469706a4826124c13eb
Arne Seime <arne.seime@gmail.com>
2022-08-18 16:23:42 +0200

    Initial commit

---
+++ src/main/java/no/entur/protobuf/numericfieldmask/NumericFieldMaskUtil.java
@@ -0,0 +1,293 @@
+package no.entur.protobuf.numericfieldmask;
+
+/*-
+ * #%L
+ * Numeric field mask for protobuf
+ * %%
+ * Copyright (C) 2022 Entur
+ * %%
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * #L%
+ */
+
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.FieldMask;
+import com.google.protobuf.GeneratedMessageV3;
+import com.google.protobuf.util.FieldMaskUtil;
+import no.entur.protobuf.NumericFieldMask;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class NumericFieldMaskUtil {
+
+	static final Map<Class, FieldMask> allFieldsMaskCache = new ConcurrentHashMap<>();
+	public static final String PATH_SEPARATOR = ".";
+
+	public static FieldMask toFieldMask(Descriptors.Descriptor protoDescriptor, NumericFieldMask mask) {
+
+		FieldMask.Builder fieldMask = FieldMask.newBuilder();
+		if (mask.getInvertMask()) {
+			mask = invertMask(protoDescriptor, mask);
+		}
+
+		// Convert numbered field paths ie "1.2" to field names
+		for (String path : mask.getFieldNumberPathList()) {
+			fieldMask.addPaths(resolveNumericPath(null, path, protoDescriptor));
+		}
+
+		// Remove redundant fields etc
+		return FieldMaskUtil.normalize(fieldMask.build());
+	}
+
+	private static NumericFieldMask invertMask(Descriptors.Descriptor messageDescriptor, NumericFieldMask mask) {
+		NumericFieldMask.Builder invertedMask = NumericFieldMask.newBuilder();
+		if (mask.getFieldNumberPathCount() == 0) {
+			// Simple inverted mask, include all fields
+			invertedMask.addAllFieldNumberPath(messageDescriptor.getFields().stream().map(e -> String.valueOf(e.getNumber())).collect(Collectors.toList()));
+		} else {
+			Tree<Integer> maskTree = buildMaskTree(mask);
+			Tree<Integer> messageTree = buildMessageTree(messageDescriptor, maskTree.getMaxDept());
+			Tree<Integer> invertedTree = messageTree.subtract(maskTree);
+			return invertedTree.toMask();
+		}
+
+		return invertedMask.build();
+
+	}
+
+	private static String resolveNumericPath(String parentPath, String subPath, Descriptors.Descriptor messageDescriptor) {
+		if (subPath.contains(PATH_SEPARATOR)) {
+			String[] segments = subPath.split("\\.");
+			String parent = segments[0];
+			String sub = subPath.substring(subPath.indexOf(PATH_SEPARATOR) + 1);
+			Descriptors.FieldDescriptor fieldDescriptor = messageDescriptor.findFieldByNumber(Integer.valueOf(parent));
+			if (fieldDescriptor.getType() == Descriptors.FieldDescriptor.Type.MESSAGE) {
+				Descriptors.Descriptor subMessageDescriptor = fieldDescriptor.getMessageType();
+				String newParentPath = buildNestedPath(new String[] { parentPath, parent });
+				return buildNestedPath(new String[] { toFieldName(messageDescriptor, Integer.parseInt(parent)),
+						resolveNumericPath(newParentPath, sub, subMessageDescriptor) });
+			} else {
+				// Not a message type to follow
+				return buildNestedPath(new String[] { toFieldName(messageDescriptor, Integer.parseInt(parent)) });
+			}
+		} else {
+
+			return buildNestedPath(new String[] { toFieldName(messageDescriptor, Integer.parseInt(subPath)) });
+		}
+
+	}
+
+	public static FieldMask allFields(Class<? extends GeneratedMessageV3> classTypeToMask, Descriptors.Descriptor protoDescriptor) {
+		// FieldMaskUtil.fromFieldNumbers uses reflection to find descriptor, avoid unnecessary calls
+		return allFieldsMaskCache.computeIfAbsent(classTypeToMask,
+				k -> FieldMaskUtil.fromFieldNumbers(k, protoDescriptor.getFields().stream().map(e -> e.getNumber()).collect(Collectors.toList())));
+	}
+
+	public static <T extends GeneratedMessageV3> T copyRequestedFields(T source, GeneratedMessageV3.Builder targetBuilder, NumericFieldMask mask) {
+		if (isAllFields(mask)) {
+			// Optimization; if all fields requested return original object
+			return source;
+		} else {
+			FieldMask requestedFieldMask = toFieldMask(source.getDescriptorForType(), mask);
+			FieldMaskUtil.merge(requestedFieldMask, source, targetBuilder);
+			return (T) targetBuilder.build();
+		}
+	}
+
+	private static boolean isAllFields(NumericFieldMask mask) {
+		return mask.getInvertMask() && mask.getFieldNumberPathCount() == 0;
+	}
+
+	private static String toFieldName(Descriptors.Descriptor protoDescriptor, int fieldNumber) {
+		return protoDescriptor.findFieldByNumber(fieldNumber).getName();
+	}
+
+	public static String buildNestedPath(int... segments) {
+		String path = Arrays.stream(segments).mapToObj(i -> ((Integer) i).toString()).collect(Collectors.joining(PATH_SEPARATOR));
+		return path;
+	}
+
+	private static String buildNestedPath(String... segments) {
+		return Stream.of(segments).filter(value -> null != value).collect(Collectors.joining(PATH_SEPARATOR));
+	}
+
+	public static Tree<Integer> buildMaskTree(NumericFieldMask mask) {
+
+		Tree<Integer> fieldTree = new Tree<>(-1);
+		List<String> fieldNumberPathList = mask.getFieldNumberPathList();
+		for (String path : fieldNumberPathList) {
+			String[] elements = path.split("\\.");
+
+
+			fieldTree.rootNode.addChildPath(Arrays.stream(elements).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList()));
+		}
+
+		return fieldTree;
+	}
+
+	public static Tree<Integer> buildMessageTree(Descriptors.Descriptor messageDescriptor, int maxDepth) {
+		Tree<Integer> fieldTree = new Tree<>(-1);
+		parseMessage(fieldTree.rootNode, messageDescriptor, maxDepth - 1);
+		return fieldTree;
+	}
+
+	private static void parseMessage(Node<Integer> parent, Descriptors.Descriptor messageDescriptor, int remainingDepth) {
+		List<Descriptors.FieldDescriptor> fields = messageDescriptor.getFields();
+		for (Descriptors.FieldDescriptor fieldDescriptor : fields) {
+			Node<Integer> fieldNode = new Node<>(fieldDescriptor.getNumber());
+			parent.children.add(fieldNode);
+			if (fieldDescriptor.getType() == Descriptors.FieldDescriptor.Type.MESSAGE) {
+				// Iterate into submessage
+				if (remainingDepth > 0) {
+					parseMessage(fieldNode, fieldDescriptor.getMessageType(), remainingDepth - 1);
+				}
+			}
+		}
+	}
+
+	public static class Tree<T extends Comparable> {
+		public Node<T> rootNode;
+
+		public Tree(T rootData) {
+			rootNode = new Node<>(rootData);
+			rootNode.value = rootData;
+			rootNode.children = new ArrayList<>();
+		}
+
+		public int getMaxDept() {
+			return rootNode.getMaxDept(0);
+		}
+
+		public Tree<T> subtract(Tree<T> treeToRemove) {
+			Tree<T> resultingTree = new Tree<T>(this.rootNode.value);
+			copyExclusiveNodes(resultingTree.rootNode, rootNode, treeToRemove.rootNode);
+
+			return resultingTree;
+		}
+
+		private void copyExclusiveNodes(Node<T> targetNode, Node<T> existingNode, Node<T> nodeToRemove) {
+			for (Node<T> child : existingNode.children) {
+				Optional<Node<T>> optionalNodeToRemove = nodeToRemove.children.stream().filter(e -> child.value.equals((e.value))).findFirst();
+				if (optionalNodeToRemove.isEmpty()) {
+					targetNode.addChild(new Node<>(child.value)); // Add without children
+				} else if (!child.equals(optionalNodeToRemove.get())) {
+					// Only if subtrees are different
+					Node<T> newTargetNode = new Node<>(child.value);
+					targetNode.addChild(newTargetNode);
+					copyExclusiveNodes(newTargetNode, child, optionalNodeToRemove.get());
+				}
+
+			}
+
+		}
+
+		public NumericFieldMask toMask() {
+			NumericFieldMask.Builder maskBuilder = NumericFieldMask.newBuilder();
+			for (Node<T> child : rootNode.children) {
+				// Start by list of children as root node has no meaning
+				buildPath(null, child, maskBuilder);
+			}
+
+			return maskBuilder.build();
+		}
+
+		private void buildPath(String parentPath, Node<T> node, NumericFieldMask.Builder mask) {
+			if (node.children.isEmpty()) {
+				mask.addFieldNumberPath(buildNestedPath(parentPath, node.value + ""));
+			} else {
+				for (Node<T> child : node.children) {
+					String path = buildNestedPath(parentPath, node.value + "");
+					buildPath(path, child, mask); // Recurse further into structure
+				}
+			}
+
+		}
+
+		@Override
+		public String toString() {
+			return "Tree{" + "rootNode=" + rootNode + '}';
+		}
+	}
+
+	public static class Node<T extends Comparable> {
+		public T value;
+
+		public Node(T value) {
+			this.value = value;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o)
+				return true;
+			if (o == null || getClass() != o.getClass())
+				return false;
+			Node<?> node = (Node<?>) o;
+			return value.equals(node.value) && children.equals(node.children);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(value, children);
+		}
+
+		private int getMaxDept(int currentDept) {
+			return children.stream().map(e -> e.getMaxDept(currentDept + 1)).mapToInt(v -> v).max().orElse(currentDept);
+		}
+
+		public List<Node<T>> children = new ArrayList<>();
+
+		public void addChildPath(List<T> pathSegments) {
+			if (pathSegments.isEmpty()) {
+				return;
+			}
+			Optional<Node<T>> optionalNode = children.stream().filter(e -> e.value.equals(pathSegments.get(0))).findFirst();
+			Node<T> child;
+			if (optionalNode.isPresent()) {
+				child = optionalNode.get();
+			} else {
+				child = new Node<>(pathSegments.get(0));
+				children.add(child);
+			}
+
+			List<T> remainingValues = new ArrayList<T>(pathSegments);
+			remainingValues.remove(0); // Processed here
+			child.addChildPath(remainingValues);
+
+			Collections.sort(children, Comparator.comparing(e -> e.value));
+		}
+
+		public void addChild(Node<T> newTargetNode) {
+			children.add(newTargetNode);
+		}
+
+		@Override
+		public String toString() {
+			return "Node{" + "children=" + children + ", value=" + value + '}';
+		}
+	}
+
+}
---
+++ src/test/java/no/entur/protobuf/numericfieldmask/NumericFieldMaskUtilTest.java
@@ -0,0 +1,182 @@
+package no.entur.protobuf.numericfieldmask;
+
+/*-
+ * #%L
+ * Numeric field mask for protobuf
+ * %%
+ * Copyright (C) 2022 Entur
+ * %%
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * #L%
+ */
+
+import com.google.protobuf.FieldMask;
+import com.google.protobuf.Timestamp;
+import no.entur.protobuf.NumericFieldMask;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class NumericFieldMaskUtilTest {
+
+	@Test
+	public void testToFieldMaskSingleLevel() {
+		FieldMask allFields = NumericFieldMaskUtil.toFieldMask(Timestamp.getDescriptor(), NumericFieldMask.newBuilder().setInvertMask(true).build());
+		assertContainsExact(allFields, "seconds", "nanos");
+
+		FieldMask onlySeconds = NumericFieldMaskUtil.toFieldMask(Timestamp.getDescriptor(),
+				NumericFieldMask.newBuilder()
+						.setInvertMask(true)
+						.addFieldNumberPath(NumericFieldMaskUtil.buildNestedPath(Timestamp.NANOS_FIELD_NUMBER))
+						.build());
+		assertContainsExact(onlySeconds, "seconds");
+
+		FieldMask onlyNano = NumericFieldMaskUtil.toFieldMask(Timestamp.getDescriptor(),
+				NumericFieldMask.newBuilder()
+						.setInvertMask(false)
+						.addFieldNumberPath(NumericFieldMaskUtil.buildNestedPath(Timestamp.NANOS_FIELD_NUMBER))
+						.build());
+		assertContainsExact(onlyNano, "nanos");
+	}
+
+	@Test
+	public void testToFieldMaskNested() {
+		FieldMask sourceContextSubField = NumericFieldMaskUtil.toFieldMask(com.google.protobuf.Type.getDescriptor(),
+				NumericFieldMask.newBuilder().addFieldNumberPath("4.1").addFieldNumberPath("5.1").build());
+		assertContainsExact(sourceContextSubField, "options.name", "source_context.file_name");
+
+	}
+
+	@Test
+	public void testToFieldMaskNestedInverted_thenRemoveParent() {
+		FieldMask sourceContextSubFieldInverted = NumericFieldMaskUtil.toFieldMask(com.google.protobuf.Type.getDescriptor(),
+				NumericFieldMask.newBuilder().addFieldNumberPath("5.1").addFieldNumberPath("4.1").addFieldNumberPath("4.2").setInvertMask(true).build());
+		assertContainsExact(sourceContextSubFieldInverted, "fields", "name", "oneofs", "syntax");
+	}
+
+	@Test
+	public void testToFieldMaskNestedInverted_whenParentHasRemainingChildren_thenKeepParent() {
+		FieldMask sourceContextSubFieldInverted = NumericFieldMaskUtil.toFieldMask(com.google.protobuf.Type.getDescriptor(),
+				NumericFieldMask.newBuilder().addFieldNumberPath("4.1").setInvertMask(true).build());
+		assertContainsExact(sourceContextSubFieldInverted, "name", "fields", "oneofs", "options.value", "source_context", "syntax");
+	}
+
+	@Test
+	public void testCachingOfFieldMasks() {
+		FieldMask fieldMask = NumericFieldMaskUtil.allFields(Timestamp.class, Timestamp.getDescriptor());
+		assertNotNull(fieldMask);
+		assertTrue(NumericFieldMaskUtil.allFieldsMaskCache.containsKey(Timestamp.class));
+
+	}
+
+	private void assertContainsExact(FieldMask allFields, String... paths) {
+		Arrays.sort(paths);
+		assertEquals(paths.length, allFields.getPathsList().size());
+		for (String path : paths) {
+			assertTrue(allFields.getPathsList().contains(path), "Missing path " + path + ", fieldMask=" + allFields.getPathsList() + ", paths=" + paths);
+		}
+
+	}
+
+	@Test
+	public void testParseMaskToTree() {
+
+		NumericFieldMask mask = NumericFieldMask.newBuilder()
+				.addFieldNumberPath("1")
+				.addFieldNumberPath("1.1")
+				.addFieldNumberPath("2.2")
+				.addFieldNumberPath("3.3.1")
+				.build();
+
+		NumericFieldMaskUtil.Tree<Integer> tree = NumericFieldMaskUtil.buildMaskTree(mask);
+
+		assertEquals(3, tree.rootNode.children.size());
+		assertEquals(tree.rootNode.children.get(0).children.size(), 1);
+		assertEquals(tree.rootNode.children.get(0).children.get(0).value, 1);
+		assertEquals(tree.rootNode.children.get(1).children.size(), 1);
+		assertEquals(tree.rootNode.children.get(1).children.get(0).value, 2);
+		assertEquals(tree.rootNode.children.get(2).children.size(), 1);
+		assertEquals(tree.rootNode.children.get(2).children.get(0).value, 3);
+		assertEquals(tree.rootNode.children.get(2).children.get(0).children.get(0).value, 1);
+
+	}
+
+	@Test
+	public void testParseMessageToTreeSingleLevel() {
+
+		int maxDepth = 1;
+
+		NumericFieldMaskUtil.Tree<Integer> tree = NumericFieldMaskUtil.buildMessageTree(com.google.protobuf.Type.getDescriptor(), maxDepth);
+
+		assertEquals(6, tree.rootNode.children.size());
+		for (int i = 0; i < 6; i++) {
+			assertEquals(0, tree.rootNode.children.get(i).children.size());
+
+		}
+		assertEquals(maxDepth, tree.getMaxDept());
+	}
+
+	@Test
+	public void testParseMessageToTreeTwoLevel() {
+
+		int maxDepth = 2;
+
+		NumericFieldMaskUtil.Tree<Integer> tree = NumericFieldMaskUtil.buildMessageTree(com.google.protobuf.Type.getDescriptor(), maxDepth);
+
+		assertEquals(6, tree.rootNode.children.size());
+		assertEquals(0, tree.rootNode.children.get(0).children.size()); // name
+		assertEquals(10, tree.rootNode.children.get(1).children.size()); // fields
+		assertEquals(0, tree.rootNode.children.get(2).children.size()); // oneofs
+		assertEquals(2, tree.rootNode.children.get(3).children.size()); // options
+		assertEquals(1, tree.rootNode.children.get(4).children.size()); // source_context
+		assertEquals(0, tree.rootNode.children.get(5).children.size()); // syntax
+
+		assertEquals(maxDepth, tree.getMaxDept());
+	}
+
+	@Test
+	public void testStringSorting() {
+		List<String> list = new ArrayList<>();
+		list.add("1");
+		list.add("2");
+		list.add("1.1");
+
+		Collections.sort(list);
+		assertEquals("1", list.get(0));
+		assertEquals("1.1", list.get(1));
+		assertEquals("2", list.get(2));
+	}
+
+	@Test
+	public void testToMask() {
+		NumericFieldMaskUtil.Tree<Integer> tree = new NumericFieldMaskUtil.Tree<>(-1);
+		tree.rootNode.addChildPath(new ArrayList<>(List.of(1))); // Wrap in ArrayList to allow modifications
+		tree.rootNode.addChildPath(new ArrayList<>(List.of(2))); // Wrap in ArrayList to allow modifications
+		tree.rootNode.addChildPath(new ArrayList<>(List.of(3))); // Wrap in ArrayList to allow modifications
+		NumericFieldMask numericFieldMask = tree.toMask();
+		assertEquals(3, numericFieldMask.getFieldNumberPathCount());
+
+	}
+
+}
